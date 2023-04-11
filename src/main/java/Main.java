72bb19159633205a7a87f1bb8597632114d2dbf7
Igor Avdonin <thathorizon@gmail.com>
2023-04-11 10:48:00 +0300

    Init commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,36 @@
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         System.out.println("24 часа Ле-Мана");
         ArrayList<Car>cars=new ArrayList<>();
+        int minSpeed=0;
+        int maxSpeed=250;
         Scanner scanner = new Scanner(System.in);
         for (int i=1;i<4;i++){
             String name=new String();
             int speed=0;
+            boolean validInput = false;
             while (name.isEmpty()) {
                 System.out.println("Напишите название " + i + " автомобиля:");
                 name = scanner.next();
             }
-            while ((speed<=0)||(speed>250)){
-                System.out.println("Напишите скорость " + name + " автомобиля(от 0 до 250км/ч):");
-                speed = scanner.nextInt();
+            System.out.println("Напишите скорость " + name + " автомобиля(от 0 до 250км/ч):");
+            while ((speed<=minSpeed)||(speed>maxSpeed)||(!validInput)){
+                if (scanner.hasNextInt()) {
+                    speed = scanner.nextInt();
+                    validInput=true;
+                }
+                else {
+                    System.out.println("Напишите скорость " + name + " автомобиля(от 0 до 250км/ч):");
+                    scanner.next();
+                    validInput=false;
+                }
             }
             cars.add(new Car(name,speed));
         }
         Race race=new Race();
-        System.out.println(race.raceLider(cars));
+        System.out.println("Победитель " + race.raceLider(cars));
     }
 }
 class Race{
@@ -38,9 +48,9 @@ class Race{
     }
 }
 class Car{
-    String name;
-    int speed;
-    public Car(String name,int speed) {
+    final String name;
+    final int speed;
+    public Car(final String name,final int speed) {
         this.name = name;
         this.speed=speed;
     }
